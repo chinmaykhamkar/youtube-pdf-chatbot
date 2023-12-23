4fa85ccb4a74916a02292d643cd83ae297ba10d4
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2023-12-23 13:23:58 +0530

    pdf chat started

--- pages/pdf_page.py
+++ pages/pdf_page.py
@@ -1,3 +1,22 @@
+import time
 import streamlit as st
+import pdf_chat as pfc
+from PyPDF2 import PdfReader
+with st.sidebar:    
+    open_ai_key = st.text_input("Enter OpenAI API key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
-st.title("pdf chat")
+def main():
+    st.title("PDF Chatbot")
+    st.caption("Chat with any pdf file")        
+    
+    pdf = st.file_uploader("upload your pdf", type='pdf')
+    if pdf is None:
+        st.info("Please upload a pdf file")
+    else:
+        pdf_read = pfc.pdf_reader(pdf)
+        st.write(pdf_read)
+
+
+if __name__ == '__main__':
+    main()
--- pdf_chat.py
+++ pdf_chat.py
@@ -1,2 +1,12 @@
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def pdf_reader(pdf):
+    pdf_read = PdfReader(pdf)
+    text = ""
+    for page in pdf_read.pages:
+        text += page.extract_text()
+        
+    return text
+    
 
